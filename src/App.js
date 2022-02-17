1f4a74146f2f413bcbb912c8b59c178815d3da97
Muhammad Abbas <anwarabbas22.22@gmail.com>
2022-02-17 17:59:48 +0500

    files changed

--- src/App.js
+++ src/App.js
@@ -1,77 +1,56 @@
-import Navbar from './Components/Navbar';
-import Alert from './Components/Alert';
-import Main from './Components/Main';
-import Contact from './Components/Contact';
-import React, { useState } from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Alert from "./Components/Alert";
+import Main from "./Components/Main";
+import Contact from "./Components/Contact";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
-  const [alert, setalert] = useState(null)
-  const showalert = (message , type)=>{
+  const [alert, setalert] = useState(null);
+  const showalert = (message, type) => {
     setalert({
-      msg : message,
-      type : type
-    })
+      msg: message,
+      type: type,
+    });
     setTimeout(() => {
-    setalert(null)
+      setalert(null);
     }, 1500);
-  }
-  const [mode, setmode] = useState('light')
+  };
+  const [mode, setmode] = useState("light");
   const toggleMode = () => {
-    if (mode === 'light') {
+    if (mode === "light") {
       removecls();
-      setmode('dark') 
-      document.body.style.backgroundColor = '#565656';
-      document.getElementById('textbox').style.backgroundColor = '#afafaf';
-      document.getElementById('textbox').style.cursor = 'white';
-      showalert('Dark mode successfully activated','Success')
-      document.title = 'Text Editor - Dark Mode'
-     
+      setmode("dark");
+      document.body.style.backgroundColor = "#565656";
+      document.getElementById("textbox").style.backgroundColor = "#afafaf";
+      document.getElementById("textbox").style.cursor = "white";
+      showalert("Dark mode successfully activated", "Success");
+      document.title = "Text Editor - Dark Mode";
     } else {
-      setmode('light')
-      document.body.style.backgroundColor = 'white';
-      document.getElementById('textbox').style.backgroundColor = 'white'
-      showalert('Light mode successfully activated','Success')
-      document.title = 'Text Editor - Light Mode'
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+      document.getElementById("textbox").style.backgroundColor = "white";
+      showalert("Light mode successfully activated", "Success");
+      document.title = "Text Editor - Light Mode";
+    }
+  };
 
-     
-    }
-  }
-  const bluishToggle = () => {
-    if(mode === 'light'){
-      setmode('dark')
-      document.body.style.background = '#295d6f';
-    }
-    else{
-      setmode('dark');
-      document.body.style.background = 'light';
-    }
-  }
   function removecls() {
-    document.body.classList.remove('bg-info')
-    document.body.classList.remove('bg-success')
-    document.body.classList.remove('bg-danger')
-    document.body.classList.remove('bg-primary')
-  }
-  function toggleColor(cls) {
-    removecls();
-    document.body.classList.add('bg-'+cls)
-    console.log(cls);
+    document.body.classList.remove("bg-info");
+    document.body.classList.remove("bg-success");
+    document.body.classList.remove("bg-danger");
+    document.body.classList.remove("bg-primary");
   }
   return (
     <Router>
-      <Navbar title='Dreamer' mode={mode} toggleMode= {toggleMode}/>
-      <Alert alert={alert}/>
+      <Navbar title="Dreamer" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <Switch>
-          <Route path="/contact">
-             <Contact mode={mode}/>
-          </Route>         
-          <Route path="/">
-            <Main mode={mode} showalert={showalert}/>
-          </Route>
+        <Route path="/contact">
+          <Contact mode={mode} />
+        </Route>
+        <Route path="/">
+          <Main mode={mode} showalert={showalert} />
+        </Route>
       </Switch>
     </Router>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default function Navbar(props) {
   return (
-    <nav style= {{backgroundColor:'blue'}} className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
-        <div className="container-fluid">
+    <nav
+      style={{ backgroundColor: "blue" }}
+      className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}
+    >
+      <div className="container-fluid">
         <Link className="navbar-brand " to="/">
           {props.title}
         </Link>
@@ -26,30 +29,21 @@ export default function Navbar(props) {
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/contact">
-               Contact
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/contact"
+              >
+                Contact
               </Link>
             </li>
           </ul>
-          <div className="d-flex">
-          <div className="bg-primary rounded mx-2" style={{height:'1.5rem',width:'1.5rem'}} onClick={()=>props.toggleColor('primary')}></div>
-          <div className="bg-success rounded mx-2" style={{height:'1.5rem',width:'1.5rem'}} onClick={()=>props.toggleColor('success')}></div>
-          <div className="bg-danger rounded mx-2" style={{height:'1.5rem',width:'1.5rem'}} onClick={()=>props.toggleColor('danger')}></div>
-          <div className="bg-info rounded mx-2" style={{height:'1.5rem',width:'1.5rem'}} onClick={()=>props.toggleColor('info')}></div>
-          </div>
-          <div className={`form-check form-switch text-${props.bluishmode === 'dark' ? 'light' : 'dark'} mx-4`} >
-            <input
-              onClick={props.bluishToggle}
-              className="form-check-input "
-              type="checkbox"
-              role="switch"
-              id="switchBlue"
-            />
-            <label className="form-check-label" htmlFor="switchBlue">
-              Switch to Bluish
-            </label>
-          </div>
-          <div className={`form-check form-switch text-${props.mode === 'dark' ? 'light' : 'dark'}`} >
+
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "dark" ? "light" : "dark"
+            }`}
+          >
             <input
               onClick={props.toggleMode}
               className="form-check-input"
